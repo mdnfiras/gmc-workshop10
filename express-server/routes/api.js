334fb8909364d19dc27d933af0cb244a201164c5
Firas Medini <mdnfiras@yahoo.com>
2021-04-10 17:51:20 +0100

    Update api.js

--- express-server/routes/api.js
+++ express-server/routes/api.js
@@ -4,10 +4,16 @@ const express = require('express');
 const router = express.Router();
 
 // MongoDB URL from the docker-compose file
-const dbHost = 'mongodb://root1:pass1@mongo-svc.web-app.svc.cluster.local:27017/users';
+const dbHost = 'mongodb://mongo-svc.web-app.svc.cluster.local/test';
 
 // Connect to mongodb
-mongoose.connect(dbHost, {useNewUrlParser: true});
+mongoose.connect(dbHost);
+
+//Get the default connection
+//var db = mongoose.connection;
+
+//Bind connection to error event (to get notification of connection errors)
+//db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // create mongoose schema
 const userSchema = new mongoose.Schema({
@@ -20,7 +26,9 @@ const User = mongoose.model('User', userSchema);
 
 /* GET api listing. */
 router.get('/', (req, res) => {
-		res.send('api works');
+	const user = new User({name: "myname", age: 77});
+	user.save();
+	res.send('api works');
 });
 
 /* GET all users. */
